d6176d671468507db3e146ba96a380a9b94091a2
martinezynwa <martin.gnebus@gmail.com>
2022-10-10 17:40:08 +0200

    server provider change

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {
 } from '@apollo/client'
 import { setContext } from 'apollo-link-context'
 
-const httpLink = new HttpLink({ uri: 'https://limitless-escarpment-24945.herokuapp.com/' })
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_BACKEND_URI })
 
 const authLink = setContext(() => {
   const token = localStorage.getItem('jwtToken')
